cc154cf99855453d08925d0bee7b6acaeb10116c
Vakho Nakashidze <vakho.nakashidze@gmail.com>
2021-03-29 22:11:39 +0400

    Animation with circles and gradients

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
 const canvas = document.getElementById('circle');
 const ctx = canvas.getContext('2d');
 
-let x = 60,
-    y = 60,
-    radius = 50,
-    // 0deg   - 1.5 * Pi,
-    // 90deg  - 0   * Pi, 
-    // 180deg - 0.5 * Pi, 
-    // 270deg - 1   * Pi
-    angleStart = 1.5 * Math.PI,
-    points = 1.5
-    
+let x = 250,
+    y = 250,
+    lineWidth = 50;
+    radius = 200,
+    percentage = 80;
+    maxPoints = 100;
+    angle = 0 - Math.PI / 2;
+    points = 0;
+    colorRatio = 0;
 
-//Create gradient
-var gradient = ctx.createLinearGradient(0,500,0, 0);
-gradient.addColorStop(0, '#59C2FF');
-gradient.addColorStop(1, '#FFDF40');
-
-
+const maxAngle = percentage * (2 * Math.PI) / 100 - Math.PI / 2;
 //Draw circle
-ctx.beginPath();
-ctx.arc(x, x, radius, 0, 2*Math.PI, false);
-ctx.lineWidth = 10;
-ctx.strokeStyle = 'rgba(200,200,200, 0.2)'
-ctx.stroke();
+function drawCircle(ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = 'rgba(200,200,200, 0.2)'
+    ctx.stroke();
+}
+
+function pickHex(color1, color2, weight) {
+    var w1 = weight;
+    var w2 = 1 - w1;
+    var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
+        Math.round(color1[1] * w1 + color2[1] * w2),
+        Math.round(color1[2] * w1 + color2[2] * w2)];
+    return rgb;
+}
+
+drawCircle(ctx);
 
 //Draw arc
 
 
 
+console.log(percentage / 100 * 360);
 setInterval(function (){
-    if(points<=3){
-        let angleEnd = points * Math.PI;
+    if(angle <= maxAngle){
+        const color = pickHex([87, 193, 255], [18, 112, 226], colorRatio)
         ctx.beginPath();
-        ctx.arc(x, y, radius, angleStart, angleEnd);
-        ctx.strokeStyle = gradient;
-        ctx.lineWidth = 10;
-        ctx.lineCap = 'round';
+        ctx.lineWidth = 1;
+        ctx.arc(x + radius * Math.cos(angle), y + radius * Math.sin(angle), lineWidth / 2, 0, 2*Math.PI);
+        ctx.fillStyle = 'rgb('+color.join()+')';
+        ctx.fill();
+        ctx.strokeStyle = 'rgb('+color.join()+')';
         ctx.stroke();
-
-        points+=0.01
+        
+        angle += Math.PI / 180;
+        colorRatio += 1 / 360 * (percentage / 100);
+        
     }
 }, 10)
 
